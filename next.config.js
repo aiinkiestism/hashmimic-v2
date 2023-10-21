93dd6421d22690575892b9df75754b8c56a607dd
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2023-10-22 07:34:21 +0900

    feat: add static build for eth.link hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const { DEPLOY_TARGET } = process.env;
+
 const nextConfig = {
   transpilePackages: ['three'],
+  output: DEPLOY_TARGET === 'fleek' ? 'export' : null,
+  distDir: 'dist',
   experimental: {
     typedRoutes: true,
   },
