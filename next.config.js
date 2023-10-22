de0790d143009765cd655ff6f7920c11ec360364
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2023-10-22 09:33:40 +0900

    fix: next config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const { DEPLOY_TARGET } = process.env;
 const nextConfig = {
   transpilePackages: ['three'],
   output: DEPLOY_TARGET === 'fleek' ? 'export' : 'standalone',
-  distDir: 'dist',
   experimental: {
     typedRoutes: true,
   },
