71036ade5b6bed07dc29436a2392ff85db4051a7
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2024-02-10 15:36:37 +0900

    feat: add rough links to Who

--- src/app/(base)/who/page.tsx
+++ src/app/(base)/who/page.tsx
@@ -1,9 +1,20 @@
 'use client'
 
 import { MainText3Ds } from "@/components";
+import { useTexture } from "@react-three/drei";
 import { Fragment } from "react";
+import TwitterIcon from "/public/twitter.svg"
+import LinkedInIcon from "/public/linkedin.svg"
+import GithubIcon from "/public/github.png"
+import SpotifyIcon from "/public/spotify-icon.png"
+import AppleMusicIcon from "/public/apple-music-icon.png"
+import YoutubeIcon from "/public/youtube-icon.png"
 
 export default function Who() {
+  const onClick = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <>
       <MainText3Ds.who.Title />
@@ -12,7 +23,32 @@ export default function Who() {
           {description()}
         </Fragment>
       ))}
-      {/* links */}
+      <group position={[0, -1, 0]}>
+        <mesh position={[-1.5, 0.5, 0]} onClick={() => onClick("https://twitter.com/hashmimic")}>
+          <boxGeometry args={[0.7, 0.7, 0.1]} />
+          <meshBasicMaterial attach="material" map={useTexture(TwitterIcon.src)} />
+        </mesh>
+        <mesh position={[0, 0.5, 0]} onClick={() => onClick("https://www.linkedin.com/in/keishi-s-665542190/")}>
+          <boxGeometry args={[0.7, 0.7, 0.1]} />
+          <meshBasicMaterial attach="material" map={useTexture(LinkedInIcon.src)} />
+        </mesh>
+        <mesh position={[1.5, 0.5, 0]} onClick={() => onClick("https://github.com/aiinkiestism")}>
+          <boxGeometry args={[0.7, 0.7, 0.1]} />
+          <meshBasicMaterial attach="material" map={useTexture(GithubIcon.src)} />
+        </mesh>
+        <mesh position={[-1.5, -0.5, 0]} onClick={() => onClick("https://open.spotify.com/artist/7M2UIn786SiNFD3VK7nxFn")}>
+          <boxGeometry args={[0.7, 0.7, 0.1]} />
+          <meshBasicMaterial attach="material" map={useTexture(SpotifyIcon.src)} />
+        </mesh>
+        <mesh position={[0, -0.5, 0]} onClick={() => onClick("https://music.apple.com/jp/artist/hashmimic/1699465976")}>
+          <boxGeometry args={[0.7, 0.7, 0.1]} />
+          <meshBasicMaterial attach="material" map={useTexture(AppleMusicIcon.src)} />
+        </mesh>
+        <mesh position={[1.5, -0.5, 0]} onClick={() => onClick("https://www.youtube.com/@hashmimic")}>
+          <boxGeometry args={[0.7, 0.7, 0.1]} />
+          <meshBasicMaterial attach="material" map={useTexture(YoutubeIcon.src)} />
+        </mesh>
+      </group>
     </>
   )
 }
